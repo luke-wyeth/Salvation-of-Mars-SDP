3478218242a6fbf51c4f2ad8ba6d5b8b87be64bd
luke-wyeth <luke.wyeth@mail.com>
2019-09-29 23:13:57 +1300

    rocket ship update
    added better resolution rocket sprite, added rocket ship script to blast off! added beginning of the final level

---
+++ salvation_of_mars/Assets/Scripts/ObstacleScripts/RocketScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    public GameObject targetPos;
+    private bool flying;
+    public GameObject player;
+    public GameObject clone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flying)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos.transform.position, Time.deltaTime * 10);
+        }
+    }
+        
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        setFlying();
+    }
+
+    private void setFlying()
+    {
+        flying = true;
+        player.SetActive(false); // disable the player
+        clone.SetActive(false); // disable the clone
+    }
+}
