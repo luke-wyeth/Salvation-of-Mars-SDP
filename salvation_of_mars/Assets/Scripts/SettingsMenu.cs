783bced790036189229a1ef9392a62433a3baf7b
weixu <155239871@qq.com>
2019-10-06 13:42:13 +1300

    SEC slider

--- salvation_of_mars/Assets/Scripts/SettingsMenu.cs
+++ salvation_of_mars/Assets/Scripts/SettingsMenu.cs
@@ -7,8 +7,15 @@ public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public AudioMixer SECMixer;
+
 	public void SetVolume(float volume)
 	{
         audioMixer.SetFloat("Volume", volume);
 	}
+
+    public void SetSECvolume(float volume)
+    {
+        SECMixer.SetFloat("SECvolume", volume);
+    }
 }
