4d36e2a52c3f791b5d0322e500708fddad75af2c
JianPineda <53285189+JianPineda@users.noreply.github.com>
2019-10-11 16:50:29 +1300

    main menu sounds
    all buttons in main menu now have on click audio

--- salvation_of_mars/Assets/Scripts/ButtonClickSound.cs
+++ salvation_of_mars/Assets/Scripts/ButtonClickSound.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonSound : MonoBehaviour
+public class ButtonClickSound : MonoBehaviour
 {
-    //Different audios for different sounds
     public AudioSource myAudio;
-    public AudioClip hoverAudio;
     public AudioClip clickAudio;
-
-    //Play audio once a button is hovered over
-    public void HoverButtonSound()
-    {
-        myAudio.PlayOneShot(hoverAudio);
-    }
 
     //Play audio once a button is clicked
     public void ClickButtonSound()
