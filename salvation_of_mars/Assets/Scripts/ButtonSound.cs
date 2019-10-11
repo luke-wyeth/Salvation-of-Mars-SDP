5b9498e92d57d8e574e696846687074a0c0f226d
JianPineda <53285189+JianPineda@users.noreply.github.com>
2019-10-11 15:25:59 +1300

    Testing Button sounds
    There is sounds when hovering over start button and clicking it.

---
+++ salvation_of_mars/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    //Different audios for different sounds
+    public AudioSource myAudio;
+    public AudioClip hoverAudio;
+    public AudioClip clickAudio;
+
+    //Play audio once a button is hovered over
+    public void HoverButtonSound()
+    {
+        myAudio.PlayOneShot(hoverAudio);
+    }
+
+    //Play audio once a button is clicked
+    public void ClickButtonSound()
+    {
+        myAudio.PlayOneShot(clickAudio);
+    }
+}
