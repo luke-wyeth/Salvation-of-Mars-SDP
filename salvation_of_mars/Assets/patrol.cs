16d27ba901c35ce482e791613c1d29d54d55378d
Breahem-Bedi <lovelybedi@hotmail.com>
2019-10-14 03:30:31 +1300

    Patroler level added

---
+++ salvation_of_mars/Assets/patrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private bool moveRight = true;
+    public Transform groundDetection;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D grounder = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        if (grounder.collider == false)
+        {
+            if (moveRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveRight = true;
+            }
+        }
+
+    }
+
+}
