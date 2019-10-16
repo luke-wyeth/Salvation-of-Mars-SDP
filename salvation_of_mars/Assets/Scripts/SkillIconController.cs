bf536e80f66a5b1e531653423119e39f66ca37f0
MatthewLuthi <mattluthi97@gmail.com>
2019-10-17 05:51:03 +1300

    Added skill icon controller script

---
+++ salvation_of_mars/Assets/Scripts/SkillIconController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillIconController : MonoBehaviour
+{
+    //public bool isBoostImgOn, isGravityImgOn, isCloneImgOn;
+    public bool isImgOn;
+    public Image boostAbilityIcon, gravityAbilityIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boostAbilityIcon.enabled = false;
+        gravityAbilityIcon.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SkillSelect.boostSelected)
+        {
+            isImgOn = true;
+            boostAbilityIcon.enabled = true;
+        }
+        else if (SkillSelect.gravitySelected)
+        {
+            isImgOn = true;
+            gravityAbilityIcon.enabled = true;
+        }
+        else if (SkillSelect.cloneSelected)
+        {
+            //isCloneImgOn = true;
+            //isBoostImgOn = false;
+            //isGravityImgOn = false;
+        }
+    }
+}
