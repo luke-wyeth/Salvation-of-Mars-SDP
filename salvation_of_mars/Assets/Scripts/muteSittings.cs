e4cb0fc3c4ecf4ae11daf5fd1dc17ec51f4335ea
weixu <155239871@qq.com>
2019-10-16 22:51:13 +1300

    unit test and sound effect

---
+++ salvation_of_mars/Assets/Scripts/muteSittings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muteSittings
+{
+    public GameObject muteControlButton;
+    public Sprite audioOffSprite;
+    public Sprite audioOnSprite;
+
+    // Start is called before the first frame update
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void muteControl()
+    {
+        if (AudioListener.pause == true)
+        {
+            AudioListener.pause = false;
+            
+        }
+        else
+        {
+            AudioListener.pause = true;
+           
+        }
+    }
+}
