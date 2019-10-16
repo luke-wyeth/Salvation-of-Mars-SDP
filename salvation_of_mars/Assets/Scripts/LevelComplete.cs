507733b6b7f30f37bda4e5dee96b6b4f914e40fd
JianPineda <53285189+JianPineda@users.noreply.github.com>
2019-10-16 16:06:42 +1300

    Level Complete menu
    Need the unlocking abilities levels to finish

---
+++ salvation_of_mars/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+   //Loads the tutorial levels corresponding to the recently unlocked ability
+
+   /*public void GoToTutorial()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "BoostUnlock")
+        {
+            SceneManager.LoadScene("BoostTutorial");
+        }
+        else if (scene.name == "GravityUnlock")
+        {
+            SceneManager.LoadScene("GravityTutorial");
+        }
+        else if (scene.name == "CloneUnlock") ;
+        {
+            SceneManager.LoadScene("CloneTutorial");
+        }
+    }*/
+}
