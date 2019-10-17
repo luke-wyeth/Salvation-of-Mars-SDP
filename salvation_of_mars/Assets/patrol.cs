5562fe1ae7070014280e9dbd11662b94b22d6875
luke-wyeth <luke.wyeth@mail.com>
2019-10-17 21:57:53 +1300

    saw animation fix
    saw animation removed because i am shit

--- salvation_of_mars/Assets/patrol.cs
+++ salvation_of_mars/Assets/patrol.cs
@@ -7,17 +7,15 @@ public class patrol : MonoBehaviour
     public float speed; // how fast the character will move.
     public float distance; // the distance character must cover.
     private bool moveRight = true; // this will be used to tell the character once the reaches the end of the platform, where it needs to move towards.
-    public Transform groundDetection; // shoots a ray on the right to detect ground collision.
-    public Transform groundDetection2; // left side ray
+    public Transform groundDetection; // shoots a ray to detect ground collision.
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime); // moves the character forward (towards right direction).
 
         RaycastHit2D grounder = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);  // creates a ray and shoots it downwards
-        RaycastHit2D grounder2 = Physics2D.Raycast(groundDetection2.position, Vector2.down, distance);
 
-        if (grounder.collider == false || grounder2.collider == false) // if rays hasn collied with anything
+        if (grounder.collider == false) // if rays hasn collied with anything
         {
             if (moveRight == true) // if character is moving right turns character in the opposite direction. 
             {
