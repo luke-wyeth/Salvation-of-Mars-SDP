03cf094b8fbe48a7f6012212e45482e3bc033b10
cheryl-cheng <54129415+cheryl-cheng@users.noreply.github.com>
2019-10-21 20:06:38 +1300

    Update ToNextScene.cs

--- salvation_of_mars/Assets/Scripts/ToNextScene.cs
+++ salvation_of_mars/Assets/Scripts/ToNextScene.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 
 public class ToNextScene : MonoBehaviour
 {
-    private int nextSceneToLoad;
+    public static float nextSceneToLoad;
+    public static float score;
+    public static bool levelFinished;
 
     void Start()
     {
         nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        levelFinished = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerInfo pi = collision.gameObject.GetComponent<PlayerInfo>();
 
         if(pi.collectedCard)
         {
-            SceneManager.LoadScene(nextSceneToLoad);
+            score = Time.timeSinceLevelLoad;
+            levelFinished = true;
+            //SceneManager.LoadScene(nextSceneToLoad);
         } 
     }
 }
