9d1024a08f5036ca94ae4ae7ecc5bda49412d6de
MatthewLuthi <mattluthi97@gmail.com>
2019-10-22 22:05:20 +1300

    Added alarm trigger functionality

---
+++ salvation_of_mars/Assets/Scripts/AlarmTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmTrigger : MonoBehaviour
+{ 
+    public PlayerInfo playerInfo, cloneInfo;
+    public Animator alarmAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInfo.collectedCard || cloneInfo.collectedCard)
+        {
+            alarmAnimator.SetBool("isTriggered", true);
+        }
+    }
+}
